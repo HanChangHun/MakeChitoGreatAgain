d43079950d6fa9f81dd1683d1ee629532ff01aca
chun <ehwjs1914@naver.com>
2021-06-11 00:07:27 +0900

    edit prevent zero or negative division

--- src/utils/midtermUtils.js
+++ src/utils/midtermUtils.js
@@ -2,7 +2,8 @@ export function getMidtermStatus(params, chito) {
     let semester = (chito.week % 16) + 1
     let health = 50 * semester
     let damage = chito.intelligence
-    let sleepy = (10 * semester) / chito.health
+    let chito_health = Math.max(1, chito.health)
+    let sleepy = (10 * semester) / chito_health
 
     params.setMaxHealth(health)
     params.setDamage(damage)
